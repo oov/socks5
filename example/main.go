c9352cca9a04768c01d867e208bd1b65f3cb6e00
oov <ilovevipper@gmail.com>
2014-01-28 08:08:05 +0900

    fix example

--- example/main.go
+++ example/main.go
@@ -6,23 +6,20 @@ import (
 )
 
 func main() {
-	users := make(map[*socks5.Conn]string)
-
 	srv := socks5.New()
 	srv.AuthNoAuthenticationRequiredCallback = func(c *socks5.Conn) error {
 		return socks5.ErrAuthenticationFailed
 	}
 	srv.AuthUsernamePasswordCallback = func(c *socks5.Conn, username, password []byte) error {
 		user := string(username)
-		users[c] = user
 		log.Printf("Welcome %v!", user)
+		c.Data = user
 		return nil
 	}
 	srv.HandleCloseFunc(func(c *socks5.Conn) {
-		if user, ok := users[c]; ok {
+		if user, ok := c.Data.(string); ok {
 			log.Printf("Goodbye %v!", user)
 		}
-		delete(users, c)
 	})
 
 	srv.ListenAndServe(":12345")
