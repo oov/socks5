d106d3252fd16a8334de2c0ad697cd292f26ee0e
oov <ilovevipper@gmail.com>
2014-01-28 10:28:43 +0900

    better example

--- example/main.go
+++ example/main.go
@@ -9,11 +9,18 @@ func main() {
 	srv := socks5.New()
 	srv.AuthUsernamePasswordCallback = func(c *socks5.Conn, username, password []byte) error {
 		user := string(username)
+		if user != "guest" {
+			return socks5.ErrAuthenticationFailed
+		}
+
 		log.Printf("Welcome %v!", user)
 		c.Data = user
 		return nil
 	}
 	srv.HandleConnectFunc(func(c *socks5.Conn, host string) (newHost string, err error) {
+		if host == "example.com:80" {
+			return host, socks5.ErrConnectionNotAllowedByRuleset
+		}
 		if user, ok := c.Data.(string); ok {
 			log.Printf("%v connecting to %v", user, host)
 		}
